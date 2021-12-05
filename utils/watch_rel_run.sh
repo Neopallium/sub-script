b98d110e72896cbd8b5838df1a167716e2bd7cb3
Robert G. Jakabosky <rjakabosky+neopallium@neoawareness.com>
2021-12-06 01:17:48 +0800

    Add ledger tests.

--- utils/watch_rel_run.sh
+++ utils/watch_rel_run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #
 
-cargo watch --no-gitignore -w init_types.json -w ./scripts/ -w ./utils/.trip/ -s "./target/release/run $@"
+cargo watch --no-gitignore -w init_types.json -w ./tests/ -w ./scripts/ -w ./utils/.trip/ -s "./target/release/run $@"
 
